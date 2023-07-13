b47cc5da850f6b468f0a7d0a6900a2923329ea3f
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-07-13 03:38:23 -0400

    location

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -18,105 +18,105 @@ class LocationSeeder extends Seeder
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '08:30',
-            'date' => '30-05-2023',
+            'date' => '2023-05-30',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '08:35',
-            'date' => '30-05-2023',
+            'date' => '05-2023-05-30',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '09:40',
-            'date' => '30-05-2023',
+            'date' => '2023-05-30',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '10:30',
-            'date' => '30-05-2023',
+            'date' => '2023-05-30',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '10:40',
-            'date' => '30-05-2023',
+            'date' => '2023-05-30',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '08:30',
-            'date' => '04-05-2023',
+            'date' => '2023-05-30',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud: 2.352222,latitud:48.856613',
             'time' => '08:35',
-            'date' => '02-05-2023',
+            'date' => '2023-05-02',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '09:40',
-            'date' => '02-05-2023',
+            'date' => '2023-05-02',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-43.172897,latitud:-22.906847',
             'time' => '10:30',
-            'date' => '01-06-2023',
+            'date' => '2023-01-06',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '10:40',
-            'date' => '11-06-2023',
+            'date' => '2023-01-11',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:139.691711,latitud:35.689487',
             'time' => '08:30',
-            'date' => '10-05-2023',
+            'date' => '2023-05-10',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '08:35',
-            'date' => '10-05-2023',
+            'date' => '2023-05-10',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '09:40',
-            'date' => '10-05-2023',
+            'date' => '2023-05-10',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '10:30',
-            'date' => '08-06-2023',
+            'date' => '2023-06-08',
             'children_id' => 1
         ]);
 
         Location::create([
             'coordinates' => 'longitud:-63.186886,latitud:-17.789218',
             'time' => '10:40',
-            'date' => '05-06-2023',
+            'date' => '2023-05-06',
             'children_id' => 1
         ]);
     }
